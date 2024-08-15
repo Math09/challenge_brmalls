398c0443bd054639aabbccf5c78f13c32fc35ffa
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-08-15 19:36:43 -0300

    feat: add annotations for Spring

--- xpto/src/main/java/br/com/brmalls/xpto/XptoApplication.java
+++ xpto/src/main/java/br/com/brmalls/xpto/XptoApplication.java
@@ -2,8 +2,13 @@ package br.com.brmalls.xpto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.webservices.client.WebServiceTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketMessagingAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = {
+		WebSocketMessagingAutoConfiguration.class,
+		WebServiceTemplateAutoConfiguration.class
+} )
 public class XptoApplication {
 
 	public static void main(String[] args) {
