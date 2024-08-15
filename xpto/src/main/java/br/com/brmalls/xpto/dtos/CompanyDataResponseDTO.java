63c1218e1d4cddd7d89c8349f0320bb6dbac4b36
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-08-15 19:51:41 -0300

    feat: create DTO for response Receita WS

---
+++ xpto/src/main/java/br/com/brmalls/xpto/dtos/CompanyDataResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.brmalls.xpto.dtos;
+
+import lombok.Data;
+
+@Data
+public class CompanyDataResponseDTO {
+
+    public String status;
+
+    public String cnpj;
+
+    public String nome;
+
+    public String fantasia;
+
+}
