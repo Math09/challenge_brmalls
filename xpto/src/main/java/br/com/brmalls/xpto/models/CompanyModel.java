ef862138991bfd7be844374c6efc6a11e3c2a30e
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-08-15 19:44:53 -0300

    feat: create company model with attributes

---
+++ xpto/src/main/java/br/com/brmalls/xpto/models/CompanyModel.java
@@ -0,0 +1,25 @@
+package br.com.brmalls.xpto.models;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table( name = "empresa" )
+@AttributeOverride( name = "id", column = @Column (name = "id") )
+public class CompanyModel extends AbstractModel {
+
+    @Column( name = "cnpj" )
+    public @Getter @Setter String cnpj;
+
+    @Column( name = "razao_social" )
+    public @Getter @Setter String socialName;
+
+    @Column( name = "nome_fantasia" )
+    public @Getter @Setter String fantasyName;
+
+}
