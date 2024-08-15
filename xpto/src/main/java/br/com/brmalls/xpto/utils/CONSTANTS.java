757a3a9e27ee89854e68e27eae7cb74e5c38ceaa
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-08-15 19:39:31 -0300

    feat: create constant file

---
+++ xpto/src/main/java/br/com/brmalls/xpto/utils/CONSTANTS.java
@@ -0,0 +1,23 @@
+package br.com.brmalls.xpto.utils;
+
+public class CONSTANTS {
+
+    public static final String URL_TAX_SERVICE = "https://www.receitaws.com.br/v1/cnpj/";
+
+    public static final String START_METHOD = "Starting method {} with CNPJ: {}";
+
+    public static final String COMPANY_SAVED = "Company saved in the database: {}";
+
+    public static final String QUERYING_COMPANY = "Querying company for CNPJ: {}";
+
+    public static final String DATA_RECEIVED = "Data received from Tax Service: {}";
+
+    public static final String RESPONSE_NO = "No response from 'Receita WS' for the CNPJ: {}";
+
+    public static final String RESPONSE_NON_OK = "Tax Service returned a non-OK status: {}";
+
+    public static final String ERROR_QUERYING = "Error querying Tax Service for CNPJ: {}";
+
+    public static final String INVALID_CNPJ = "Invalid CNPJ: {}";
+
+}
