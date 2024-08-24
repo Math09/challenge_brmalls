3bcd55502774424cf0f4d2a309a0574f327f2df4
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-08-24 02:02:45 -0300

    test(util) create unit tests for valid cnpj

--- xpto/src/test/java/br/com/brmalls/xpto/utils/CNPJUtilsTest.java
+++ xpto/src/test/java/br/com/brmalls/xpto/utils/CNPJUtilsTest.java
@@ -1,2 +1,42 @@
-package br.com.brmalls.xpto.utils;public class CNPJUtilsTest {
+package br.com.brmalls.xpto.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CNPJUtilsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = { "CNPJ_PLACEHOLDER", "CNPJ_PLACEHOLDER" } )
+    void testValidCNPJ( String cnpj ) {
+        assertTrue( CNPJUtils.isValidCNPJ( cnpj ) );
+    }
+
+    @Test
+    void isInvalidCNPJForLength() {
+        final String invalidCNPJ = "CNPJ_PLACEHOLDER"; // CNPJ with less than 14 digits.
+        assertFalse( CNPJUtils.isValidCNPJ( invalidCNPJ ) );
+    }
+
+    @Test
+    void isInvalidCNPJIncorrectDigits() {
+        final String invalidCNPJ = "CNPJ_PLACEHOLDER"; // CNPJ with incorrect check digits.
+        assertFalse( CNPJUtils.isValidCNPJ( invalidCNPJ ) );
+    }
+
+    @Test
+    void isInvalidCNPJInputMismatchException() {
+        final String invalidCNPJ = "CNPJ_PLACEHOLDER"; // CNPJ with an invalid character (letter 'A').
+        assertFalse( CNPJUtils.isValidCNPJ( invalidCNPJ ) );
+    }
+
+    @Test
+    void validateCNPJWithFormatting() {
+        final String formattedCNPJ = "CNPJ_PLACEHOLDER"; // CNPJ with formatting.
+        assertTrue( CNPJUtils.isValidCNPJ( formattedCNPJ ) );
+    }
+
 }
