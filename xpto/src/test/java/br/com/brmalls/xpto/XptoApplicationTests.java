55afe6d1b62ca5e798b9838fc6212034ef363ca6
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-08-31 21:52:42 -0300

    test: remove comments for annotations

--- xpto/src/test/java/br/com/brmalls/xpto/XptoApplicationTests.java
+++ xpto/src/test/java/br/com/brmalls/xpto/XptoApplicationTests.java
@@ -3,10 +3,10 @@ package br.com.brmalls.xpto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 class XptoApplicationTests {
 
-	// @Test
+	@Test
 	void contextLoads() {
 	}
 
