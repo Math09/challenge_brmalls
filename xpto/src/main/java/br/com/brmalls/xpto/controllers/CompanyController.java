01eb557903909a96bdb32bc186e5557ba28f20a0
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-09-03 21:12:44 -0300

    style(controller): speficy imports

--- xpto/src/main/java/br/com/brmalls/xpto/controllers/CompanyController.java
+++ xpto/src/main/java/br/com/brmalls/xpto/controllers/CompanyController.java
@@ -3,7 +3,11 @@ package br.com.brmalls.xpto.controllers;
 import br.com.brmalls.xpto.models.CompanyModel;
 import br.com.brmalls.xpto.services.CompanyService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping( "/company" )
